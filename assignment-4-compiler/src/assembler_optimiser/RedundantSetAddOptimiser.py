775e9e39abb6073d0408595ace1eabf8ee1ad7bd
Mateusz <sshozye@gmail.com>
2023-01-28 21:42:09 +0100

    upgraded setadd optimiser

--- assignment-4-compiler/src/assembler_optimiser/RedundantSetAddOptimiser.py
+++ assignment-4-compiler/src/assembler_optimiser/RedundantSetAddOptimiser.py
@@ -1,9 +1,9 @@
-from ..common.commands import gen_load, gen_add
+from ..common.commands import gen_load, gen_add, SET
 from ..common.cfg_models import BasicBlock
 from ..control_flow_graph.cfg import ICFG
 
 
-class RedundantSet0Optimiser:
+class RedundantSetAddOptimiser:
     def __init__(self, icfg: ICFG):
         self.icfg = icfg
 
@@ -23,6 +23,10 @@ class RedundantSet0Optimiser:
             if prev_command.directive == "SET" and prev_command.arg == "0" and command.is_add:
                 new_commands.append(gen_load(command.is_i)(command.arg, prev_command.label))
                 i += 2
+            elif prev_command.directive == "SET" and prev_command.arg.isdecimal() and command.is_add and command.arg.startswith("*const_"):
+                const_num = int(command.arg.split("const_")[1][:-1])
+                new_commands.append(SET(str(int(prev_command.arg) + const_num), prev_command.label))
+                i+=2
             else:
                 new_commands.append(prev_command)
                 i += 1
--- assignment-4-compiler/src/assembler_optimiser/runner.py
+++ assignment-4-compiler/src/assembler_optimiser/runner.py
@@ -1,6 +1,6 @@
 from .EquivalenceLoadAddSwapper import EquivalenceLoadAddSwapper
 from .RedundantLoadOptimiser import RedundantLoadOptimiser
-from .RedundantSet0Optimiser import RedundantSet0Optimiser
+from .RedundantSetAddOptimiser import RedundantSetAddOptimiser
 from ..control_flow_graph.cfg import ICFG
 
 
@@ -10,6 +10,6 @@ def optimise_assembler(icfg: ICFG):
     RedundantLoadOptimiser(icfg).optimise()
     EquivalenceLoadAddSwapper(icfg).swap()
     RedundantLoadOptimiser(icfg).optimise()
-    RedundantSet0Optimiser(icfg).optimise()
+    RedundantSetAddOptimiser(icfg).optimise()
 
 
